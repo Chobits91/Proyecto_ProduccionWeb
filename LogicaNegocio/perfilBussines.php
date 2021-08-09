d27de9feecabfdce024b7134937566b8b93af7a3
Stefania Sanchiz <stefaniasanchiz@gmail.com>
2021-08-09 17:07:06 -0300

    PERFILES: listado y form (no funciona).

--- LogicaNegocio/perfilBussines.php
+++ LogicaNegocio/perfilBussines.php
@@ -2,20 +2,42 @@
 
 require_once('../DataAccess/PerfilDAO.php');
 
-class PerfilBusiness{
+class PerfilBusiness
+{
 
     protected $PerfilDAO;
 
-    function __construct($con){
+    function __construct($con)
+    {
         $this->PerfilDAO = new PerfilDAO($con);
     }
 
-    public function getPerfiles(){
-        $users = $this->PerfilDAO->getAll(); 
+    public function getPerfil($id_perfil)
+    {
+
+        $perfil = $this->PerfilDAO->getOne($id_perfil);
+        return $perfil;
+    }
+
+    public function getPerfiles()
+    {
+        $users = $this->PerfilDAO->getAll();
 
         return $users;
     }
 
-    
+    public function savePerfil($datos)
+    {
+        $this->PerfilDAO->save($datos);
+    }
 
+    public function modifyPerfil($id, $datos)
+    {
+        $this->PerfilDAO->modify($id, $datos);
+    }
+
+    public function deletePerfil($id)
+    {
+        $this->PerfilDAO->delete($id);
+    }
 }
